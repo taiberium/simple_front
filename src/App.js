f7b8d3dff33d206ba3cfcd7e2553de429a17111f
Бебенин Виктор Владимирович <Bebenin.V.Vl@sberbank.ru>
2019-10-24 11:43:42 +0300

    version 1.0.1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          app version 1.0.0
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {version: ''};
+    }
+    componentDidMount() {
+        axios.get('getVersion')
+            .then((data, err) => {
+                console.log(data);
+                this.setState({...data.data});
+                if (err) {
+                    console.error(err);
+                }
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        app version {this.state.version}
+                    </p>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
